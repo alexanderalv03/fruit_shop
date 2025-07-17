96900ed09609c9980c2f04212269e0b1bf887ff9
Alexander <al_emeric@hotmail.com>
2025-07-17 17:08:02 -0300

    update

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_shop/src/auth/components/custom_test_field.dart';
 
@@ -10,29 +11,150 @@ class SignInScreen extends StatelessWidget {
       backgroundColor: Colors.green,
       body: Column(
         children: [
-          Expanded(child: Container(color: Colors.red)),
           Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-
-              decoration: const BoxDecoration(
-                color: Colors.white,
-
-                borderRadius: BorderRadius.vertical(top: Radius.circular(45)),
-              ),
-              child: Column(
-                children: [
-                  //Email
-                  CustomTextField(icon: Icons.email, label: 'Email'),
-
-                  //Password
-                  CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Password',
-                    isObscure: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //App name
+                const Text.rich(
+                  TextSpan(
+                    style: TextStyle(fontSize: 40),
+                    children: [
+                      TextSpan(
+                        text: 'Green',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'grocer',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+
+                //Category
+                SizedBox(
+                  height: 30,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(fontSize: 25),
+                    child: AnimatedTextKit(
+                      pause: Duration.zero,
+                      repeatForever: true,
+                      animatedTexts: [
+                        FadeAnimatedText('Frutas'),
+
+                        FadeAnimatedText('Verduras'),
+
+                        FadeAnimatedText('Legumes'),
+
+                        FadeAnimatedText('Carnes'),
+
+                        FadeAnimatedText('Cereais'),
+
+                        FadeAnimatedText('Laticinios'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          //FORM
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+
+            decoration: const BoxDecoration(
+              color: Colors.white,
+
+              borderRadius: BorderRadius.vertical(top: Radius.circular(45)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //Email
+                const CustomTextField(icon: Icons.email, label: 'Email'),
+
+                //Password
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Password',
+                  isSecret: true,
+                ),
+
+                //ENTER BUTTON
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Enter', style: TextStyle(fontSize: 18)),
+                  ),
+                ),
+
+                //FORGOT PASSWORD
+                Align(
+                  alignment: Alignment.centerRight,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot password?',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+
+                //DIVISOR
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //NEW USER BUTTON
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      side: BorderSide(width: 2, color: Colors.green),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Create account',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
---
+++ lib/src/config/custom_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> _swatchOpacity = {
+
+  50: const Color.fromRGBO(139, 195, 74, 0.1),
+  100: const Color.fromRGBO(139, 195, 74, 0.1),
+  200: const Color.fromRGBO(139, 195, 74, 0.1),
+  300: const Color.fromRGBO(139, 195, 74, 0.1),
+  400: const Color.fromRGBO(139, 195, 74, 0.1),
+  500: const Color.fromRGBO(139, 195, 74, 0.1),
+  600: const Color.fromRGBO(139, 195, 74, 0.1),
+  700: const Color.fromRGBO(139, 195, 74, 0.1),
+  800: const Color.fromRGBO(139, 195, 74, 0.1),
+  900: const Color.fromRGBO(139, 195, 74, 0.1),
+
+};
+
+abstract class CustomColors {
+  static Color customContrastColor = Colors.red.shade700;
+
+    static MaterialColor customSwatchColor = MaterialColor(0xFF8BC34A  ,)
+}
